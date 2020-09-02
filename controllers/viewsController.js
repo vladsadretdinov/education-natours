713e429ced5f91006ba6cef5f4aaac940bdeafcb
Vladislav Sadretdinov <vlad.sadretdinov@gmail.com>
2020-09-02 23:18:17 +0300

    Done 183 lesson

---
+++ controllers/viewsController.js
@@ -0,0 +1,31 @@
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build template
+
+  // 3) Render that template using tourdata from 1)
+
+  res.status(200).render('overview', {
+    title: 'All tours',
+    tours: tours,
+  });
+});
+
+exports.getTour = catchAsync(async (req, res) => {
+  // 1) get the data, for the requested tour(including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  // 2) Build template
+  // 3) Render template usinge data from 1)
+  res.status(200).render('tour', {
+    title: 'Single Tour',
+    tour: tour,
+  });
+});
