45a64688dee15d359ecd5588562ca62235255ffa
Vladislav Sadretdinov <vlad.sadretdinov@gmail.com>
2020-09-05 18:50:51 +0300

    done

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -41,6 +42,8 @@ app.use('/api', limiter);
 
 // Body pareser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
---
+++ public/js/login.js
@@ -0,0 +1,31 @@
+/* eslint-disable */
+
+const login = async (email, password) => {
+  try {
+    console.log(email, password);
+    const res = await axios({
+      method: 'POST',
+      url: 'http://localhost:3000/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
+
+    if (res.data.status === 'success') {
+      alert('Logged in successfuly');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (err) {
+    alert(err.response.data.message);
+  }
+};
+
+document.querySelector('.form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  login(email, password);
+});
